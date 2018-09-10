dbe3e5af8ad58da5cd9af46afc9131cc6eec8861
devC-mcu <43113509+devC-mcu@users.noreply.github.com>
2018-09-11 00:25:21 +0530

    Add files via upload

---
+++ music playback using sd card.ino
@@ -0,0 +1,21 @@
+#include "SD.h"         // sd card libraries
+#define SD_ChipSelectPin 4
+#include "TMRpcm.h"
+#include "SPI.h"
+
+TMRpcm tmrpcm;
+
+void setup(){
+tmrpcm.speakerPin = 9;       // pin for aduio output connected to the speaker
+Serial.begin(9600);
+if (!SD.begin(SD_ChipSelectPin)) {
+Serial.println("SD fail");
+return;
+}
+
+tmrpcm.setVolume(5);          // sets the music volume
+tmrpcm.play("test.wav");      // plays the music file named test.wav from sd card
+}
+// audio will be played once on every reset
+void loop(){  }
+
